4f2a6a6657e98a1d6c976e624de028f0f6028663
ElitsaDev <elica__andreeva@abv.bg>
2022-08-13 10:30:04 +0300

    Final project

--- src/test/java/bg/softuni/artgalleryshop/service/UserRoleServiceTest.java
+++ src/test/java/bg/softuni/artgalleryshop/service/UserRoleServiceTest.java
@@ -3,31 +3,34 @@ package bg.softuni.artgalleryshop.service;
 import bg.softuni.artgalleryshop.model.entity.UserRoleEntity;
 import bg.softuni.artgalleryshop.model.enums.UserRoleEnum;
 import bg.softuni.artgalleryshop.repository.UserRoleRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserRoleServiceTest {
     @Mock
     private UserRoleRepository mockUserRoleRepository;
-
-    private UserRoleEnum userRoleEnum;
     @InjectMocks
     private UserRoleService toTest;
-
     private UserRoleEntity userRoleEntity;
+
+    private UserRoleEnum userRoleEnum;
 
     @BeforeEach
     void setUp(){
         toTest = new UserRoleService(mockUserRoleRepository);
         userRoleEntity = new UserRoleEntity();
+        userRoleEntity.setUserRole(UserRoleEnum.USER);
 
     }
 
@@ -40,14 +43,15 @@ public class UserRoleServiceTest {
 
         );
     }
+    @Test
+    void findByRoleNameDoesntExist(){
+        userRoleEnum = UserRoleEnum.valueOf("USER");
+        when(mockUserRoleRepository.findByUserRole(UserRoleEnum.USER)).thenReturn(Optional.empty());
+        toTest.initializeRoles();
 
-//    @Test
-//    void initializeRoles_IfNotExist(){
-//        toTest.initializeRoles();
-//        userRoleEntity.setUserRole(UserRoleEnum.ADMIN);
-//       UserRoleEntity userRole = mockUserRoleRepository.save(userRoleEntity.setId(1l));
-//
-//        Assertions.assertEquals("ADMIN",userRole.getUserRole().name());
-//
-//    }
+        Assertions.assertNotEquals(userRoleEnum,mockUserRoleRepository.findByUserRole(UserRoleEnum.USER));
+
+    }
+
+
 }
